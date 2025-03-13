2034aae7786f2d5720921672d7033c74cfe49a20
Emile Decosterd <edautomation20@gmail.com>
2025-03-13 10:54:56 +0100

    Update reference to application project

--- base_external/package/final_project/final_project.mk
+++ base_external/package/final_project/final_project.mk
@@ -5,14 +5,14 @@
 #
 ##############################################################
 
-FINAL_PROJECT_VERSION = '947202347c777023cf4fcc2dd78ebe10e7063b55'
+FINAL_PROJECT_VERSION = '7a1f2714d430266b0993aaef304a23b5731ca076'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 FINAL_PROJECT_SITE = 'git@github.com:edautomation/embedded-linux-final-project.git'
 FINAL_PROJECT_SITE_METHOD = git
 FINAL_PROJECT_GIT_SUBMODULES = YES
-# FINAL_PROJECT_MODULE_SUBDIRS = serial_control
+FINAL_PROJECT_MODULE_SUBDIRS = serial_driver
 
 define FINAL_PROJECT_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/serial_control all
@@ -20,7 +20,9 @@ endef
 
 define FINAL_PROJECT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/serial_control/serial_control $(TARGET_DIR)/usr/bin/serial_control
+	$(INSTALL) -m 0755 $(@D)/serial_driver/serial_driver_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/serial_driver/serial_driver_unload $(TARGET_DIR)/usr/bin/
 endef
 
-# $(eval $(kernel-module))
+$(eval $(kernel-module))
 $(eval $(generic-package))
