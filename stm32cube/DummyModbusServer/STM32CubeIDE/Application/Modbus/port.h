37302ab5d531d105db64fee5e39738a6c96769de
edautomation <122728655+edautomation@users.noreply.github.com>
2025-03-20 07:32:03 +0100

    feat(stm32): Dummy modbus server (#18)
    - Dummy modbus server with 64 registers starting at address 0.
    - Supports reading and writing holding registers commands

---
+++ stm32cube/DummyModbusServer/STM32CubeIDE/Application/Modbus/port.h
@@ -0,0 +1,27 @@
+#ifndef NANOMODBUS_CONFIG_H
+#define NANOMODBUS_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NMBS_RTU
+
+#define REG_BUF_SIZE 64
+
+#include "nanomodbus.h"
+
+typedef struct tNmbsServer
+{
+    uint8_t id;
+    uint16_t regs[REG_BUF_SIZE];
+} nmbs_server_t;
+
+nmbs_error nmbs_server_init(nmbs_t* nmbs, nmbs_server_t* server);
+nmbs_error nmbs_client_init(nmbs_t* nmbs);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
